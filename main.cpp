78f37e5fec1cd25bdf59e487a73c18126cfeacc8
yo_kanyukari <e1n17075@st.oit.ac.jp>
2020-03-14 02:36:51 +0900

    fix: refuse IPv4 communication

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <cstdio>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 
 int main(int argc, char const *argv[])
 {
     std::cout << "start" << std::endl;
 
-    int sockfd, len, s, port = 20000, pid;
-    struct sockaddr_in listen_IPv4, sender_IPv4;
+    int sockfd, len, s, port = 8080, mtu = 1500, pid;
+    sockaddr_in listen_IPv4, sender_IPv4;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -19,28 +21,36 @@ int main(int argc, char const *argv[])
 
     if (bind(sockfd, (struct sockaddr *)&listen_IPv4, sizeof(listen_IPv4)) == -1)
     {
-        perror("bind");
+        std::perror("bind");
         return EXIT_FAILURE;
     }
 
     listen(sockfd, 5);
-
+    len = sizeof(sender_IPv4);
+    
     while (1)
     {
-        s = accept(sockfd, (struct sockaddr *)&sender_IPv4, (socklen_t *)sizeof(sender_IPv4));
-
+        s = accept(sockfd, (struct sockaddr *)&sender_IPv4, (socklen_t *)&len);
+        //プロセス分ける
         pid = fork();
+        std::cout << s << " " << std::endl;
 
+        //子プロセスの内容
         if (pid == 0)
         {
+            std::string packet;
+            //accept()で異常がないか確認する
             if (s < 0)
             {
-                perror("accept");
+                std::perror("accept");
                 return EXIT_FAILURE;
             }
 
+            //接続元IP、PORTを表示する
             std::cout << inet_ntoa(sender_IPv4.sin_addr) << " " << ntohs(sender_IPv4.sin_port) << std::endl;
 
+            //ブラウザから送られてきたGETメソッドを受け取る
+            recv(s, &packet, mtu, 0);
             close(s);
         }
         else
@@ -50,8 +60,8 @@ int main(int argc, char const *argv[])
         }
     }
     close(sockfd);
-    
-    std::cout<< "end" << std::endl;
 
-    return 0;
+    std::cout << "end" << std::endl;
+
+    return EXIT_SUCCESS;
 }
